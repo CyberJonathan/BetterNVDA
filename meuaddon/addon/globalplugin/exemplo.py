11317d9a69ee43dc094ebbd400c95c1aa38c5c6d
Jonathan Santana <166652495+CyberJonathan@users.noreply.github.com>
2025-10-22 22:27:58 -0300

    Adição das pastas NVDA
    Adição das pastas principais da Extensão pro NVDA. GlobalPlugin e suas conectadas

---
+++ meuaddon/addon/globalplugin/exemplo.py
@@ -0,0 +1,15 @@
+import globalPluginHandler 
+import ui
+import keyboardHandler
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+    def __init__(self):
+
+        super().__init__()
+        ui.message("NVDABetter esta ativo")
+
+        def script_digaOElemento(self, gesture):
+            focusObject = ui.getFocusObject()
+            if focusObject:
+                ui.message(f"Elemento focado é: {focusObject.name}")
+            self.scrpit_digaOElemento = script_digaOElemento
---
+++ meuaddon/buildVars.py
@@ -0,0 +1,6 @@
+addon_info = {
+    "addon_name": "exemplo",
+    "addon_summary": "Extensão de teste para NVDA",
+    "addon_version": "1.0",
+    "addon_author": "Jonathan Lima",
+}
